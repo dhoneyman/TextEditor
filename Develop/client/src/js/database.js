65ba3d8fceaed0cde8bf8d63370fe0179fbbe425
Dylan Honeyman <dylan@dylans-iMac.local>
2022-01-24 17:28:21 -0600

    pwa working

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -3,19 +3,36 @@ import { openDB } from 'idb';
 const initdb = async () =>
   openDB('jate', 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
+      if (db.objectStoreNames.contains('jateOS')) {
         console.log('jate database already exists');
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      db.createObjectStore('jateOS', { keyPath: 'id', autoIncrement: true });
       console.log('jate database created');
     },
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.error('putDb not implemented');
+  const todosDb = await openDB('jate', 1);
+  const tx = todosDb.transaction('jateOS', 'readwrite');
+  const store = tx.objectStore('jateOS');
+  const request = store.put({ value: content, id: 1 });
+  const result = await request;
+  console.log('🚀 - data saved to the database', result);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.error('getDb not implemented');
+  const todosDb = await openDB('jate', 1);
+  const tx = todosDb.transaction('jateOS', 'readonly');
+  const store = tx.objectStore('jateOS');
+  const request = store.get(1);
+  const result = await request;
+  console.log('result.value', result);
+  return result?.value;
+}
 
 initdb();
